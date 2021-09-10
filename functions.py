6baef10fcc4fcec2f3eb02ad8ea8672b10695be8
DataHill82467 <89399001+DataHill82467@users.noreply.github.com>
2021-09-10 11:14:38 +0200

    'rename'

--- functions.py
+++ functions.py
@@ -1,7 +1,6 @@
 import webbrowser
 
 
-####
 def get_stock_tickers():
     
     '''
@@ -24,4 +23,24 @@ def get_stock_tickers():
     return(lst)
 
 
-####
+
+def read_data_file(file_name):
+    
+    """Function to read in data from a txt file. The txt file should have
+    one number (float) per line. The numbers are stored in the data attribute.
+                
+    Args:
+    file_name (string): name of a file to read from
+        
+    Returns:
+        None
+        
+    """
+            
+    with open(file_name) as file:
+        data_list = []
+        line = file.readline()
+        while line:
+            data_list.append(int(line))
+            line = file.readline()
+    file.close()
