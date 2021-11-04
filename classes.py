9f55d02e2bc9a97d2576509019d704741ae52cc4
jakobreinhardt <89399001+DataHill82467@users.noreply.github.com>
2021-11-04 13:58:51 +0100

    'edit'

--- classes.py
+++ classes.py
@@ -1,24 +1,11 @@
-# Classes
 
 class Asset:
     '''
-    Parent Asset Class
+    Parent Class Asset
     '''
     
-    def __init__(self, price=0, marketcap=0):
+    def __init__(self, price, marketcap):
         '''
-        
-        Parameters
-        ----------
-        price : TYPE
-            DESCRIPTION.
-        marketcap : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        None.
-
         '''
 
         self.price = price
@@ -30,21 +17,49 @@ class Stock(Asset):
     Inheritance Class Stock
     '''
     
-    def __init__(self, price, marketcap):
+    def __init__(self, price, marketcap, isin, amount):
         '''
-        
-
         Parameters
         ----------
-        price : TYPE
-            DESCRIPTION.
-        marketcap : TYPE
-            DESCRIPTION.
-
-        Returns
-        -------
-        None.
+        price : price of one share
+        marketcap :marketcap of the company
+        isin : Isin number of the stock
+        amount : Number of Stocks in Portfolio
 
         '''
         
         Asset.__init__(self, price, marketcap)
+        self.isin = isin
+        self.amount = amount
+        
+    def stocksplit(self, splitfactor):
+        '''
+        Changes Stock amount and Stock price according to splitfactor
+        
+        input: splitfactor
+        return: None
+        '''
+        
+        self.amount = self.amount * splitfactor
+        self.price = self.price/ splitfactor
+        
+    
+        
+class Gold(Asset):
+    '''
+    Inheritance Class Gold
+    '''
+    
+    def __init__(self, price, marketcap, quality, amount):
+        '''
+        Parameters
+        ----------
+        price : price for one ounce
+        marketcap :marketcap of Gold
+        quality : Quality label of the Gold
+        amount : Number of ounces
+        '''
+        Asset.__init__(self, price, marketcap)
+        self.quality = quality
+        self.amount = amount
+                
